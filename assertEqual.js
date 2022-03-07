e9a4f075b996373a50822c90e683448476ac5ed0
Krismina <krismina.la@gmail.com>
2022-03-07 22:57:48 +0000

    wk01d01 create our own assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+const emoji = require('node-emoji');
+const yes = emoji.get('white_check_mark');
+const no = emoji.get('red_circle');
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(yes + yes + yes + " Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(no + no + no + " Assertion Failed: " + actual + " !=== " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
