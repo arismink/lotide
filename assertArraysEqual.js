2821ce4065b91461f2f43a7de8e5f8c492eb426d
Krismina <krismina.la@gmail.com>
2022-03-09 01:05:06 +0000

    create a function that uses eqArrays.js and checks two arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,4 @@
+const { eq } = require('lodash');
 const emoji = require('node-emoji');
 const yes = emoji.get('white_check_mark');
 const no = emoji.get('red_circle');
@@ -5,19 +6,19 @@ const no = emoji.get('red_circle');
 const eqArrays = function(arr1, arr2) {
   for (let x = 0; x < arr1.length; x++) {
     if (arr1[x] !== arr2[x]) {
-      return false;
-    }
+      return false
+    };
   }
   return true;
 };
 
-const assertArraysEqual = function(test) {
-  if (test) {
-    console.log(`${yes}${yes}${yes} Assertion Passed`);
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`${yes}${yes}${yes} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`${no}${no}${no} Assertion Failed`);
+    console.log(`${no}${no}${no} Assertion Failed: ${actual} !== ${expected}`);
   }
 
-}
+};
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]));
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
