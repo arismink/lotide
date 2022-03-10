068c5be1199e5fb803296124c760c148f711a60e
Krismina <krismina.la@gmail.com>
2022-03-10 03:33:44 +0000

    function that returns key of specifiedvalue

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const yes = "✅";
+const no = "🔴";
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${yes}${yes}${yes} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${no}${no}${no} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(searchObject, searchKey) {
+  for (let object in searchObject) {
+    if (searchObject[object] === searchKey) {
+      return object;
+    }
+  }
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
