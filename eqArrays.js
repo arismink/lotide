2d772fc32736d474f76168dd727bd8339c40e9ce
Krismina <krismina.la@gmail.com>
2022-03-10 07:22:11 +0000

    function that checks if two arrays are equal

--- eqArrays.js
+++ eqArrays.js
@@ -3,12 +3,16 @@ const yes = emoji.get('white_check_mark');
 const no = emoji.get('red_circle');
 
 const eqArrays = function(arr1, arr2) {
-  for (let x = 0; x < arr1.length; x++) {
-    if (arr1[x] !== arr2[x]) {
-      return false
-    };
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let x = 0; x < arr1.length; x++) {
+      if (arr1[x] !== arr2[x]) {
+        return false;
+      }
+    }
+    return true;
   }
-  return true;
 };
 
 const assertEqual = function(actual, expected) {
@@ -20,4 +24,6 @@ const assertEqual = function(actual, expected) {
 
 };
 
-assertEqual(eqArrays([1, 3, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 3, 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
