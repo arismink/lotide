fcda5f507263af91bdf40192e506c4c6bacd9c23
krismina la <krismina.la@gmail.com>
2022-03-14 22:44:07 +0000

    refactored function and exported it so it can be used in middleTest

--- middle.js
+++ middle.js
@@ -1,26 +1,7 @@
-// 4:45pm
-// 5:20pm
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let x = 0; x < arr1.length; x++) {
-      if (arr1[x] !== arr2[x]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 // function returns array of middle element
 
@@ -37,15 +18,8 @@ const middle = function(numArray) {
         answer.push(numArray[x])
       }
     }
-  } else {
-    return answer
-  }
+  } 
   return answer
 }
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), [] );
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4] );
+module.exports = middle;
