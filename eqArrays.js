a2d82bf6ebef3219de095f7c72c3f453187a2642
krismina la <krismina.la@gmail.com>
2022-03-14 22:35:49 +0000

    refactored function. turned if statements with no else in to 1 line

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,11 @@
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) { // guard clause
-    return false;
-  }
+  if (arr1.length !== arr2.length) return false;
   
   for (let x = 0; x < arr1.length; x++) {
     if (Array.isArray(arr1[x]) && Array.isArray(arr2[x])) {
-      if (!eqArrays(arr1[x], arr2[x])) {
-        return false
-      }
-    } else if (arr1[x] !== arr2[x]) {
-      return false;
+      if (!eqArrays(arr1[x], arr2[x])) return false;
     }
+    if (arr1[x] !== arr2[x]) return false;
   }
   return true;
 };
