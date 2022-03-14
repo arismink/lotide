5a1ab754fb2c411234a0103a31ac6afea6c1f554
krismina la <krismina.la@gmail.com>
2022-03-14 03:13:20 +0000

    added extra examples to demonstrate object recursion check

--- eqObjects.js
+++ eqObjects.js
@@ -77,3 +77,5 @@ assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true) // =>
 assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false) // => false
 
 assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
+
+assertEqual(eqObjects({ a: { z: 1 , d: { f: 2 } }, b: 2 }, { a: { z: 1 , d: { f: 2 } }, b: 2 }), true) // => false
