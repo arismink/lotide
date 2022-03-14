e7933cd4bf16dc5d41f2651de63098c1fb1b2dbe
krismina la <krismina.la@gmail.com>
2022-03-14 22:56:34 +0000

    fixed if statement bug where it returns a false true

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,10 @@
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
+  if (arr1.length !== arr2.length) return false; // guard clause
   
   for (let x = 0; x < arr1.length; x++) {
     if (Array.isArray(arr1[x]) && Array.isArray(arr2[x])) {
       if (!eqArrays(arr1[x], arr2[x])) return false;
-    }
-    if (arr1[x] !== arr2[x]) return false;
+    } else if (arr1[x] !== arr2[x]) return false;
   }
   return true;
 };
