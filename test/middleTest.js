9188ec8ed399c77d6c96ea32c80ee654826b72bb
krismina la <krismina.la@gmail.com>
2022-03-15 01:08:34 +0000

    made mocha test for middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,23 @@
+const { expect } = require('chai');
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), [] );
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4] );
+describe("#middle", () => {
+  it("returns [] when passed [1]", () => {
+    expect(middle([1])).to.be.empty;
+  });
+  it("returns [] when passed [1, 2]", () => {
+    expect(middle([1, 2])).to.be.empty;
+  });
+  it("returns [2] when passed [1, 2, 3]", () => {
+    expect(middle([1, 2, 3])).to.deep.equal([2])
+  });
+  it("returns [3] when passed [1, 2, 3, 4, 5]", () => {
+    expect(middle([1, 2, 3, 4, 5])).to.deep.equal([3])
+  });
+  it("returns [2, 3] when passed [1, 2, 3, 4]", () => {
+    expect(middle([1, 2, 3, 4])).to.deep.equal([2, 3])
+  });
+  it("returns [3, 4] when passed [1, 2, 3, 4, 5, 6]", () => {
+    expect(middle([1, 2, 3, 4, 5, 6])).to.deep.equal([3, 4])
+  });
+})
