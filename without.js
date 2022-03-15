d0d969b2071d0bc375afe92f50527feea8120f11
krismina la <krismina.la@gmail.com>
2022-03-15 03:27:23 +0000

    refactored and exported function

--- without.js
+++ without.js
@@ -1,42 +1,10 @@
 // implement a function that will return the subset of a given array, removing unwanted elements
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let x = 0; x < arr1.length; x++) {
-      if (arr1[x] !== arr2[x]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const without = function(words, delWords) {
   let newWords = [];
 
-  for (let char of words) {
-    if (!delWords.includes(char)) {
-      newWords.push(char);
-    }
-  }
-  return newWords
+  for (let char of words) if (!delWords.includes(char)) newWords.push(char);
+
+  return newWords;
 }
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-// test code
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+module.exports = without;
