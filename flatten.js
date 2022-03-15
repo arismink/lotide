fbeede1abea7af3d5f57ef82c895c5eb60cf793e
krismina la <krismina.la@gmail.com>
2022-03-15 03:26:57 +0000

    refactored and exported function

--- flatten.js
+++ flatten.js
@@ -1,34 +1,6 @@
-/*
-  TASK:
-
-  create a function that will take an array containing elements, including nested arrays of elements, and return a 'flattened' version of the array.
-
-*/
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let x = 0; x < arr1.length; x++) {
-      if (arr1[x] !== arr2[x]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const flatten = function(arr) {
   let newArr = [];
-  arr.forEach(function(char) {
+  arr.forEach(char => {
     if (!Array.isArray(char)) {
       newArr.push(char);
     } else {
@@ -40,4 +12,4 @@ const flatten = function(arr) {
   return newArr;
 };
 
-assertArraysEqual(flatten([1, 2, [3, 4, 6], 5, [6]]), [1, 2, 3, 4, 6, 5, 6]);
+module.exports = flatten;
