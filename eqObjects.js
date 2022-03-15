18ccf55502f44050a0eeb357226e9de14f405a57
krismina la <krismina.la@gmail.com>
2022-03-15 03:26:15 +0000

    refactored and exported function

--- eqObjects.js
+++ eqObjects.js
@@ -1,27 +1,6 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let x = 0; x < arr1.length; x++) {
-      if (arr1[x] !== arr2[x]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) { // guard clause
@@ -49,33 +28,4 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
-
-// const cd = { c: ["1", 3], d: [3, 3] };
-// const dc = { c: ["1", 3], d: [2, 3] };
-// assertEqual(eqObjects(cd, dc), false); // => false
-
-const ef = { c: "1", d: ["2", 3, 4, [1]] };
-const fe2 = { c: "1", d: ["2", 3, 4, [1]] }
-assertEqual(eqObjects(ef, fe2), true)
-
-assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true) // => true
-
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false) // => false
-
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
-
-assertEqual(eqObjects({ a: { z: 1 , d: { f: 2 } }, b: 2 }, { a: { z: 1 , d: { f: 2 } }, b: 2 }), true) // => false
+module.export = eqObjects;
