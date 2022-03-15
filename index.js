3ca5bbf9c11a89a61e720303c170a910ad1ecc56
krismina la <krismina.la@gmail.com>
2022-03-15 03:28:23 +0000

    file exporting all functions so it can be reused globally within the project

---
+++ index.js
@@ -0,0 +1,34 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  findKey,
+  findKeyByValue,
+  flatten,
+  map,
+  letterPositions,
+  takeUntil,
+  without
+};
